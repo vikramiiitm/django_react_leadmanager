27acf838d8f2e8f57879f874b052c23030358c21
vikramiiitm <vikramiiitmc@gmail.com>
2021-02-14 17:42:07 +0530

    added react frontend

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+   return (
+      <div>
+         This from apps
+      </div>
+   )
+}
+
+ReactDOM.render(<App/>,document.getElementById("app"))
